5caaa79c1c3d385e8f2f1fdfe531ceff8ac88658
tttsaurus <chenh1231y@gmail.com>
2025-06-02 23:55:38 +0800

    Fixes

--- src/main/java/com/tttsaurus/saurus3d/mixin/early/TextureMapMixin.java
+++ src/main/java/com/tttsaurus/saurus3d/mixin/early/TextureMapMixin.java
@@ -2,6 +2,7 @@ package com.tttsaurus.saurus3d.mixin.early;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.tttsaurus.saurus3d.Saurus3D;
 import com.tttsaurus.saurus3d.common.core.gl.exception.GLIllegalStateException;
 import com.tttsaurus.saurus3d.mcpatches.api.extra.ITextureAtlasSpriteExtra;
 import com.tttsaurus.saurus3d.mcpatches.api.extra.ITextureMapExtra;
@@ -12,10 +13,9 @@ import com.tttsaurus.saurus3d.mcpatches.api.texturemap.TexUpdatePlan;
 import com.tttsaurus.saurus3d.mcpatches.impl.texturemap.TextureUploaderV1;
 import com.tttsaurus.saurus3d.mcpatches.impl.texturemap.TextureUploaderV2;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.texture.Stitcher;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraftforge.fml.common.ProgressManager;
+import net.minecraft.client.resources.IResourceManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -50,58 +50,83 @@ public class TextureMapMixin implements ITextureMapExtra
     private Map<TexRect, List<TextureAtlasSprite>> saurus3D$mergedAnimatedSprites;
 
     @Unique
-    private List<ITextureUploader> saurus3D$textureUploader;
+    private List<ITextureUploader> saurus3D$textureUploaders;
 
     @Shadow
     @Final
     protected List<TextureAtlasSprite> listAnimatedSprites;
 
-    @Inject(method = "finishLoading", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/ForgeHooksClient;onTextureStitchedPost(Lnet/minecraft/client/renderer/texture/TextureMap;)V", shift = At.Shift.AFTER), remap = false)
-    private void afterFinishLoading(Stitcher stitcher, ProgressManager.ProgressBar bar, int j, int k, CallbackInfo ci)
+    @Inject(method = "loadTextureAtlas", at = @At("RETURN"))
+    private void afterLoadTextureAtlas(IResourceManager resourceManager, CallbackInfo ci)
     {
-        if (saurus3D$enableBatchTexUpload)
+        if (saurus3D$enableBatchTexUpload && saurus3D$mergedAnimatedSprites == null)
         {
-            if (saurus3D$mergedAnimatedSprites == null)
+            List<TexRect> rects = new ArrayList<>();
+            for (TextureAtlasSprite sprite: listAnimatedSprites)
+                rects.add(((ITextureAtlasSpriteExtra)sprite).getRect());
+
+            List<TexRect> result = RectMergeAlgorithm.mergeRects(rects);
+
+            saurus3D$mergedAnimatedSprites = new HashMap<>();
+            for (TexRect r: result)
             {
-                List<TexRect> rects = new ArrayList<>();
+                List<TextureAtlasSprite> sprites = new ArrayList<>();
+                saurus3D$mergedAnimatedSprites.put(r, sprites);
                 for (TextureAtlasSprite sprite: listAnimatedSprites)
-                    rects.add(((ITextureAtlasSpriteExtra)sprite).getRect());
+                    if (r.contains(((ITextureAtlasSpriteExtra)sprite).getRect()))
+                        sprites.add(sprite);
+            }
 
-                List<TexRect> result = RectMergeAlgorithm.mergeRects(rects);
+            int count = 0;
+            for (List<TextureAtlasSprite> list: saurus3D$mergedAnimatedSprites.values())
+                count += list.size();
+            if (count != listAnimatedSprites.size())
+                throw new GLIllegalStateException("TextureMap post-texture-stitching merging algorithm ran into a problem.");
 
-                saurus3D$mergedAnimatedSprites = new HashMap<>();
-                for (TexRect r: result)
+            Saurus3D.LOGGER.info(String.format("Merged %d animated textures into %d rectangles.", count, saurus3D$mergedAnimatedSprites.size()));
+
+            saurus3D$textureUploaders = new ArrayList<>();
+
+            // 4 extra bytes to avoid potential overflow
+            int bufferSize = 4;
+            for (TexRect rect: saurus3D$mergedAnimatedSprites.keySet())
+                bufferSize += rect.width * rect.height * 4;
+
+            for (TexRect rect: saurus3D$mergedAnimatedSprites.keySet())
+            {
+                int data = rect.width * rect.height * 4;
+
+                StringBuilder builder = new StringBuilder();
+                builder.append(String.format("Animated texture rectangle: x=%d, y=%d, width=%d, height=%d; Data size: %d bytes; ",
+                        rect.x,
+                        rect.y,
+                        rect.width,
+                        rect.height,
+                        data));
+
+                if (data < 64 * 1024)
                 {
-                    List<TextureAtlasSprite> sprites = new ArrayList<>();
-                    saurus3D$mergedAnimatedSprites.put(r, sprites);
-                    for (TextureAtlasSprite sprite: listAnimatedSprites)
-                        if (r.contains(((ITextureAtlasSpriteExtra)sprite).getRect()))
-                            sprites.add(sprite);
+                    builder.append("Using TextureUploaderV1");
+                    TextureUploaderV1 uploader = new TextureUploaderV1();
+                    uploader.init(bufferSize);
+                    saurus3D$textureUploaders.add(uploader);
+                }
+                else if (data < 512 * 1024)
+                {
+                    builder.append("Using TextureUploaderV2 Double Buffering");
+                    TextureUploaderV2 uploader = new TextureUploaderV2();
+                    uploader.init(bufferSize, 2);
+                    saurus3D$textureUploaders.add(uploader);
+                }
+                else
+                {
+                    builder.append("Using TextureUploaderV2 Triple Buffering");
+                    TextureUploaderV2 uploader = new TextureUploaderV2();
+                    uploader.init(bufferSize, 3);
+                    saurus3D$textureUploaders.add(uploader);
                 }
 
-                int count = 0;
-                for (List<TextureAtlasSprite> list: saurus3D$mergedAnimatedSprites.values())
-                    count += list.size();
-                if (count != listAnimatedSprites.size())
-                    throw new GLIllegalStateException("TextureMap post-texture-stitching merging algorithm ran into a problem.");
-
-                saurus3D$textureUploader = new ArrayList<>();
-
-                // 4 extra bytes to avoid potential overflow
-                int bufferSize = 4;
-                for (TexRect rect: saurus3D$mergedAnimatedSprites.keySet())
-                {
-                    bufferSize += rect.width * rect.height * 4;
-                    saurus3D$textureUploader.add(new TextureUploaderV2());
-                }
-
-                for (ITextureUploader uploader: saurus3D$textureUploader)
-                {
-                    if (uploader instanceof TextureUploaderV1 v1)
-                        v1.init(bufferSize);
-                    else if (uploader instanceof TextureUploaderV2 v2)
-                        v2.init(bufferSize, 2);
-                }
+                Saurus3D.LOGGER.info(builder.toString());
             }
         }
     }
@@ -125,7 +150,7 @@ public class TextureMapMixin implements ITextureMapExtra
         {
             TexRect rect = entry.getKey();
             List<TextureAtlasSprite> sprites = entry.getValue();
-            ITextureUploader uploader = saurus3D$textureUploader.get(index);
+            ITextureUploader uploader = saurus3D$textureUploaders.get(index);
 
             boolean abort = false;
             uploader.reset();
