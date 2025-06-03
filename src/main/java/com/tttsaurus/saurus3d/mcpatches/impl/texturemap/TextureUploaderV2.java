3dfa551df77895bffc1842d00d30a5c1ce3239c6
tttsaurus <chenh1231y@gmail.com>
2025-06-03 16:34:54 +0800

    TreeMap cache for TextureUploaderV2

--- src/main/java/com/tttsaurus/saurus3d/mcpatches/impl/texturemap/TextureUploaderV2.java
+++ src/main/java/com/tttsaurus/saurus3d/mcpatches/impl/texturemap/TextureUploaderV2.java
@@ -25,6 +25,16 @@ public final class TextureUploaderV2 implements ITextureUploader
 
     private final Map<Integer, List<int[]>> mipmapData = new TreeMap<>();
     private final Map<Integer, List<TexRect>> mipmapRect = new TreeMap<>();
+    private List<int[]> mipmapDataCache0;
+    private List<int[]> mipmapDataCache1;
+    private List<int[]> mipmapDataCache2;
+    private List<int[]> mipmapDataCache3;
+    private List<int[]> mipmapDataCache4;
+    private List<TexRect> mipmapRectCache0;
+    private List<TexRect> mipmapRectCache1;
+    private List<TexRect> mipmapRectCache2;
+    private List<TexRect> mipmapRectCache3;
+    private List<TexRect> mipmapRectCache4;
 
     private final Map<Integer, CompletableFuture<?>> mergingProcesses = new HashMap<>();
     private boolean skipFirstTick;
@@ -71,17 +81,113 @@ public final class TextureUploaderV2 implements ITextureUploader
     {
         mipmapData.clear();
         mipmapRect.clear();
+        mipmapDataCache0 = null;
+        mipmapDataCache1 = null;
+        mipmapDataCache2 = null;
+        mipmapDataCache3 = null;
+        mipmapDataCache4 = null;
+        mipmapRectCache0 = null;
+        mipmapRectCache1 = null;
+        mipmapRectCache2 = null;
+        mipmapRectCache3 = null;
+        mipmapRectCache4 = null;
     }
 
     public void planTexUpload(int level, int[] data, TexRect rect)
     {
-        if (!mipmapData.containsKey(level))
+        boolean putMipmapData;
+        if (mipmapDataCache0 != null && level == 0)
+            putMipmapData = false;
+        else if (mipmapDataCache1 != null && level == 1)
+            putMipmapData = false;
+        else if (mipmapDataCache2 != null && level == 2)
+            putMipmapData = false;
+        else if (mipmapDataCache3 != null && level == 3)
+            putMipmapData = false;
+        else if (mipmapDataCache4 != null && level == 4)
+            putMipmapData = false;
+        else
+            putMipmapData = !mipmapData.containsKey(level);
+
+        if (putMipmapData)
             mipmapData.put(level, new ArrayList<>());
-        if (!mipmapRect.containsKey(level))
+
+        boolean putMipmapRect;
+        if (mipmapRectCache0 != null && level == 0)
+            putMipmapRect = false;
+        else if (mipmapRectCache1 != null && level == 1)
+            putMipmapRect = false;
+        else if (mipmapRectCache2 != null && level == 2)
+            putMipmapRect = false;
+        else if (mipmapRectCache3 != null && level == 3)
+            putMipmapRect = false;
+        else if (mipmapRectCache4 != null && level == 4)
+            putMipmapRect = false;
+        else
+            putMipmapRect = !mipmapRect.containsKey(level);
+
+        if (putMipmapRect)
             mipmapRect.put(level, new ArrayList<>());
 
-        mipmapData.get(level).add(data);
-        mipmapRect.get(level).add(new TexRect(rect.x >> level, rect.y >> level, rect.width >> level, rect.height >> level));
+        List<int[]> d;
+        List<TexRect> r;
+
+        switch (level)
+        {
+            case 0 ->
+            {
+                if (mipmapDataCache0 == null)
+                    mipmapDataCache0 = mipmapData.get(level);
+                d = mipmapDataCache0;
+                if (mipmapRectCache0 == null)
+                    mipmapRectCache0 = mipmapRect.get(level);
+                r = mipmapRectCache0;
+            }
+            case 1 ->
+            {
+                if (mipmapDataCache1 == null)
+                    mipmapDataCache1 = mipmapData.get(level);
+                d = mipmapDataCache1;
+                if (mipmapRectCache1 == null)
+                    mipmapRectCache1 = mipmapRect.get(level);
+                r = mipmapRectCache1;
+            }
+            case 2 ->
+            {
+                if (mipmapDataCache2 == null)
+                    mipmapDataCache2 = mipmapData.get(level);
+                d = mipmapDataCache2;
+                if (mipmapRectCache2 == null)
+                    mipmapRectCache2 = mipmapRect.get(level);
+                r = mipmapRectCache2;
+            }
+            case 3 ->
+            {
+                if (mipmapDataCache3 == null)
+                    mipmapDataCache3 = mipmapData.get(level);
+                d = mipmapDataCache3;
+                if (mipmapRectCache3 == null)
+                    mipmapRectCache3 = mipmapRect.get(level);
+                r = mipmapRectCache3;
+            }
+            case 4 ->
+            {
+                if (mipmapDataCache4 == null)
+                    mipmapDataCache4 = mipmapData.get(level);
+                d = mipmapDataCache4;
+                if (mipmapRectCache4 == null)
+                    mipmapRectCache4 = mipmapRect.get(level);
+                r = mipmapRectCache4;
+            }
+            default ->
+            {
+                d = mipmapData.get(level);
+                r = mipmapRect.get(level);
+            }
+        }
+
+        d.add(data);
+        r.add(new TexRect(rect.x >> level, rect.y >> level, rect.width >> level, rect.height >> level));
     }
 
     private final Map<Integer, int[]> mergedDataContainer = new HashMap<>();
